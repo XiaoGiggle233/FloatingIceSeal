b0a0391355521e1d244103a2fea92ec4f00537b6
XiaoGiggle233 <w309628370@163.com>
2026-08-15 14:11:27 +0800

    重置按钮快捷键以及长按逻辑

--- Assets/Scripts/Buttons/ResetLevelButton.cs
+++ Assets/Scripts/Buttons/ResetLevelButton.cs
@@ -3,9 +3,38 @@ using UnityEngine.UI;
 
 public class ResetLevelButton : MonoBehaviour
 {
+    [SerializeField] private KeyCode resetKey = KeyCode.R;
+    [SerializeField] private float holdDuration = 0.5f;
+
+    private float holdTimer;
+    private bool hasTriggered;
+
     private void Awake()
     {
         GetComponent<Button>().onClick.AddListener(OnClick);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(resetKey))
+        {
+            if (hasTriggered)
+            {
+                return;
+            }
+
+            holdTimer += Time.deltaTime;
+            if (holdTimer >= holdDuration)
+            {
+                hasTriggered = true;
+                OnClick();
+            }
+        }
+        else
+        {
+            holdTimer = 0f;
+            hasTriggered = false;
+        }
     }
 
     private void OnClick()
